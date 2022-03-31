9894219f5eec76e4bde2f760b6d3784cd01459c1
michaelberish <mikeyberish@gmail.com>
2022-03-31 16:14:55 -0700

    done

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const colors = ["green", "red", "rgb(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
+btn.addEventListener("click", (e) => {
+    const randomNumber = getRandomNumber();
+    color.textContent = colors[randomNumber];
+    document.body.style.background = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
