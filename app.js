c5e4a3a629a00218c3892942e03fb3a7ce855479
michaelberish <mikeyberish@gmail.com>
2022-04-06 16:24:04 -0700

    done

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const colors = ["green", "red", "rgb(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// const colors = ["green", "red", "rgb(133,122,200)", "#f15025"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
 
-btn.addEventListener("click", (e) => {
-    const randomNumber = getRandomNumber();
-    color.textContent = colors[randomNumber];
-    document.body.style.background = colors[randomNumber];
-})
+// btn.addEventListener("click", (e) => {
+//     const randomNumber = getRandomNumber();
+//     color.textContent = colors[randomNumber];
+//     document.body.style.background = colors[randomNumber];
+// })
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
-}
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * colors.length);
+// }
+
+$(document).ready(function() {
+    const colors = ["green", "red", "rgb(133,122,200)", "#f15025"];
+    const btn = $("#btn");
+    const color = $(".color");
+
+    btn.on("click", function(e) {
+        const randomNumber = getRandomNumber();
+        color.text(colors[randomNumber]);
+        $("body").css("background-color", colors[randomNumber]);
+    });
+
+    function getRandomNumber() {
+        return Math.floor(Math.random() * colors.length);
+    }
+
+});
--- hex.js
+++ hex.js
@@ -1,17 +1,36 @@
-const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "D", "E", "F"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "D", "E", "F"];
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
 
-btn.addEventListener("click", (e) => {
-    let hexColor = "#";
-    for (let i = 0; i < 6; i++) {
-        hexColor += hex[getRandomNumber()];
+// btn.addEventListener("click", (e) => {
+//     let hexColor = "#";
+//     for (let i = 0; i < 6; i++) {
+//         hexColor += hex[getRandomNumber()];
+//     }
+
+//     color.textContent = hexColor;
+//     document.body.style.background = hexColor;
+// })
+
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * hex.length);
+// }
+$(document).ready(function() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "D", "E", "F"];
+    const btn = $("#btn");
+    const color = $(".color");
+
+    btn.on("click", function(e) {
+        let hexColor = "#";
+        for (let i = 0; i < 6; i++) {
+            hexColor += hex[getRandomNumber()];
+        }
+        color.text(hexColor);
+        $("body").css("background-color", hexColor);
+    });
+
+    function getRandomNumber() {
+        return Math.floor(Math.random() * hex.length);
     }
 
-    color.textContent = hexColor;
-    document.body.style.background = hexColor;
-})
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length);
-}
+});
